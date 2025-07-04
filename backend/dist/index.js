1a07c09227e96e53d52dcd1528f2c535b5b3f3b2
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-07-04 10:40:40 +0530

    compiled backend js

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -12,17 +12,58 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config();
+require("dotenv").config();
 const sdk_1 = __importDefault(require("@anthropic-ai/sdk"));
+const prompts_1 = require("./prompts");
+const express_1 = __importDefault(require("express"));
+const react_1 = require("./defaults/react");
+const react_2 = require("./defaults/react");
 const anthropic = new sdk_1.default();
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const msg = yield anthropic.messages.create({
-            model: "claude-opus-4-20250514",
-            max_tokens: 1024,
-            messages: [{ role: "user", content: "What is the capital of India?" }],
-        });
-        console.log(msg);
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.post('/template', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const prompt = req.body.prompt;
+    const response = yield anthropic.messages.create({
+        model: "claude-opus-4-20250514",
+        max_tokens: 1024,
+        system: "Return either node or react based on what do you think this project should be. Only return a simple word either 'node' or 'react'. Do not return anything extra.",
+        messages: [
+            {
+                role: "user",
+                content: prompt,
+            },
+        ],
     });
-}
-main();
+    const answer = response.content[0].text;
+    if (answer === "react") {
+        res.json({
+            prompts: [prompts_1.BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${react_1.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+            uiPrompts: [react_1.basePrompt]
+        });
+        return;
+    }
+    if (answer === "node") {
+        res.json({
+            prompts: [`Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${react_2.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+            uiPrompts: [react_2.basePrompt]
+        });
+        return;
+    }
+}));
+app.post("/chat", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    const messages = req.body.messages;
+    const response = yield anthropic.messages.create({
+        messages: messages,
+        model: 'claude-3-5-sonnet-20241022',
+        max_tokens: 8000,
+        system: (0, prompts_1.getSystemPrompt)()
+    });
+    console.log(response);
+    res.json({
+        response: (_a = response.content[0]) === null || _a === void 0 ? void 0 : _a.text
+    });
+}));
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
